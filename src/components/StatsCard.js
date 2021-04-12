28b5fc627d224540db90cf533fa5bd66583ff5c4
GannaHap <gannahap02@gmail.com>
2021-04-12 18:21:27 +0700

    Change Format Number

--- src/components/StatsCard.js
+++ src/components/StatsCard.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import axios from 'axios';
 import moment from 'moment';
-import numeral from 'numeral';
 
 import Logo from '../assets/logo.png';
 import '../css/StatsCard.css';
@@ -14,18 +13,55 @@ export default class StatsCard extends Component {
     this.state = {
       data: null,
       timeUpdated: null,
+      totalPostive: null,
+      totalRecovered: null,
+      totalDeaths: null,
+      newPositive: null,
+      newRecovered: null,
+      newDeaths: null,
     };
   }
 
   componentDidMount() {
     axios.get('https://api.covid19api.com/summary').then((doc) => {
       let dateTime = moment(doc.data.Date).subtract(1, 10).format('LLL');
+      let totalConfirmed = this.handleFormatNumber(doc.data.Global.TotalConfirmed);
+      let totalRecovered = this.handleFormatNumber(doc.data.Global.TotalRecovered);
+      let totalDeaths = this.handleFormatNumber(doc.data.Global.TotalDeaths);
+      let newPositive = this.handleFormatNumber(doc.data.Global.NewConfirmed);
+      let newRecovered = this.handleFormatNumber(doc.data.Global.NewRecovered);
+      let newDeaths = this.handleFormatNumber(doc.data.Global.NewDeaths);
       this.setState({
         data: doc.data,
         timeUpdated: dateTime,
+        totalPostive: totalConfirmed,
+        totalRecovered: totalRecovered,
+        totalDeaths: totalDeaths,
+        newPositive: newPositive,
+        newRecovered: newRecovered,
+        newDeaths: newDeaths,
       });
     });
   }
+
+  handleFormatNumber = (number) => {
+    if (number) {
+      let formatNumber = '';
+      let numberrev = number.toString().split('').reverse().join('');
+      for (let i = 0; i < numberrev.length; i++) {
+        if (i % 3 === 0) {
+          formatNumber += numberrev.substr(i, 3) + ' ';
+        }
+      }
+      formatNumber = formatNumber
+        .split('', formatNumber.length - 1)
+        .reverse()
+        .join('');
+      return formatNumber;
+    } else {
+      return null;
+    }
+  };
 
   render() {
     return (
@@ -45,7 +81,7 @@ export default class StatsCard extends Component {
             <div className="section-top">
               <div className="top-text">
                 <h2>POSITIVE</h2>
-                <span>{this.state.data && numeral(this.state.data.Global.TotalConfirmed).format('0,0')}</span>
+                <span>{this.state.totalPostive && this.state.totalPostive}</span>
               </div>
               <div className="icon">
                 <i className="fal fa-heart-rate"></i>
@@ -53,7 +89,7 @@ export default class StatsCard extends Component {
             </div>
             <div className="section-bottom finance-down">
               <i className="far fa-arrow-up"></i>
-              <span className="percent">{this.state.data && numeral(this.state.data.Global.NewConfirmed).format('0,0')}</span>
+              <span className="percent">{this.state.newPositive && this.state.newPositive}</span>
               <span className="text">Today</span>
             </div>
           </div>
@@ -62,7 +98,7 @@ export default class StatsCard extends Component {
             <div className="section-top">
               <div className="top-text">
                 <h2>RECOVERED</h2>
-                <span>{this.state.data && numeral(this.state.data.Global.TotalRecovered).format('0,0')}</span>
+                <span>{this.state.totalRecovered && this.state.totalRecovered}</span>
               </div>
               <div className="icon">
                 <i className="fas fa-first-aid"></i>
@@ -70,7 +106,7 @@ export default class StatsCard extends Component {
             </div>
             <div className="section-bottom finance-down">
               <i className="far fa-arrow-up"></i>
-              <span className="percent">{this.state.data && numeral(this.state.data.Global.NewRecovered).format('0,0')}</span>
+              <span className="percent">{this.state.newRecovered && this.state.newRecovered}</span>
               <span className="text">Today</span>
             </div>
           </div>
@@ -79,7 +115,7 @@ export default class StatsCard extends Component {
             <div className="section-top">
               <div className="top-text">
                 <h2>DEATHS</h2>
-                <span>{this.state.data && numeral(this.state.data.Global.TotalDeaths).format('0,0')}</span>
+                <span>{this.state.totalDeaths && this.state.totalDeaths}</span>
               </div>
               <div className="icon">
                 <i className="fas fa-tombstone-alt"></i>
@@ -87,7 +123,7 @@ export default class StatsCard extends Component {
             </div>
             <div className="section-bottom finance-down">
               <i className="far fa-arrow-up"></i>
-              <span className="percent">{this.state.data && numeral(this.state.data.Global.NewDeaths).format('0,0')}</span>
+              <span className="percent">{this.state.newDeaths && this.state.newDeaths}</span>
               <span className="text">Today</span>
             </div>
           </div>
